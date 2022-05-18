ed85ff55225396660f6cbf6dfce2229273e95165
Alim Khan <90334104+Mh-Alim@users.noreply.github.com>
2022-05-18 23:43:32 +0530

    this is final chat app commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const http = require("http")
+const express = require("express");
+const cors = require("cors")
+const socketIo = require("socket.io")
+
+const port = 4500 || process.env.PORT;
+const app = express();
+
+const users = [{}];
+app.get("/",(req,res)=>{
+    res.send("runnign on this roo")
+})
+
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on("connection",(socket)=>{
+    console.log("new connection ");
+    
+
+    socket.on('joined',(data)=>{
+        users[socket.id] = data.user;
+        console.log(`${data.user} is joined`);
+        socket.broadcast.emit('userJoined',{user:"Admin",message:`${users[socket.id]} has joined`})
+        socket.emit('Welcome',{user:"Admin",message: `Welcome to the chat.${users[socket.id]}`})
+    })
+    
+    socket.on('message',({message,id})=>{
+        io.emit('sendMessage',{user:users[id],message,id});
+    });
+
+
+    socket.on('disconnect',()=>{
+        socket.broadcast.emit('leave',{user:"Admin",message:`${users[socket.id]}  has left`});
+      console.log(`user left`);
+  })
+    // broadcast me user ko chodke baaki sabhi ko message jaayega
+    
+})
+
+server.listen(port,()=>{
+    console.log(`server is listening on http://localhost:${port}`);
+})
