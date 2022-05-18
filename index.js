860acbb3fd3d8c8661e165ed4c237fafab5a4d3e
Alim Khan <90334104+Mh-Alim@users.noreply.github.com>
2022-05-19 00:05:08 +0530

    make it better

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors")
 const socketIo = require("socket.io")
 
-const port = porcess.env.PORT || 4500;
+const port = process.env.PORT || 4500;
 const app = express();
 
 const users = [{}];
